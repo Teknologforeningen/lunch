6b440bdc80a402ace52dc861774e9f5ee24074f0
Jan Nyberg <jan.nyberg@gmail.com>
2021-11-04 18:38:38 +0200

    Added commented ig feed

--- client/src/components/public/PublicApp.js
+++ client/src/components/public/PublicApp.js
@@ -6,6 +6,7 @@ import PublicMenu from './PublicMenu';
 import PublicPosts from './PublicPosts';
 import PublicPrices from './PublicPrices';
 import PublicFooter from './PublicFooter';
+import {Helmet} from "react-helmet";
 import './PublicApp.scss';
 
 const PublicApp = () => {
@@ -20,6 +21,16 @@ const PublicApp = () => {
                     < PublicMenu/>
                     < PublicPosts/>
                     < PublicPrices/>
+                    {/* <div className="ig-container">
+                        <div className="taggbox-container">
+                            <div className="taggbox-socialwall" data-wall-id="76051" view-url="https://widget.taggbox.com/76051">
+                            </div>
+                            <Helmet>
+                            <script src="https://widget.taggbox.com/embed.min.js" type="text/javascript"></script>
+                            </Helmet>
+                        </div>
+                    </div> */}
+                    
                 </div>
             < PublicFooter/>
             </div>
