51691f3bb0d1d2dba65403616eec49ee4d0870bf
Jonas Bäck <jonas.c.back@gmail.com>
2022-02-23 21:11:29 +0200

    Fix script to create admin user

--- server/src/createAdmin.js
+++ server/src/createAdmin.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('./models/User');
 const genPasswordHash = require('./utils').genPasswordHash;
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 const mongoUrl = process.env.MONGODB_USER && process.env.MONGODB_PW
     ? `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PW}@${process.env.DBURL}/${process.env.DBNAME}?authSource=admin`
@@ -15,30 +15,18 @@ mongoose.connect(mongoUrl).then(() => {
 
 const saltHash = genPasswordHash(process.env.ADMIN_PW);
 
+User.deleteMany({}, () => console.log('Old users deleted'));
 const salt = saltHash.salt;
 const hash = saltHash.hash;
-
-User.findOne({ 'username': process.env.ADMIN_USER }, (err, user) => {
-    if (err) {
-        return res.status(500).json({
-            title: 'server error',
-            error: err
-        });
-    } else if (!user) {
-        const newUser = new User({
-            username: process.env.ADMIN_USER,
-            hash: hash,
-            salt: salt
-        });
-        newUser.save(err => {
-            if (err) {
-                console.log('Could not save user');
-            } else {
-                console.log('User saved successfully');
-            }
-        });
-        
-    } else {
-        console.log('NOP');
-    }
+const username = process.env.ADMIN_USER;
+const newUser = new User({
+    username: username,
+    hash: hash,
+    salt: salt
+});
+newUser.save().then(result => {
+    console.log(`${username} saved successfully`);
+    mongoose.connection.close();
+}).catch(error => {
+    console.log(`Could not save user: ${error}`)
 });
